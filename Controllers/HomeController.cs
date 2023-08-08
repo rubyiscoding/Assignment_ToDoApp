865092d44b5527fbf54f3d3305da0521b8c7a6e4
RubyIsCoding <ruby.chaulagain@yahoo.com>
2023-08-08 10:47:07 -0400

    Created MVC core project and added Sqlite db to retrieve list of todos.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+using ToDoApp.Data;
+using ToDoApp.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+using ToDoApp.Models;
+
+namespace ToDoApp.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ToDoContext _db;
+
+        public HomeController(ILogger<HomeController> logger, ToDoContext toDoContext)
+        {
+            _logger = logger;
+            _db= toDoContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View(GetToDos());
+        }
+
+        private List<ToDo> GetToDos(){
+            var abc = _db.ToDos.ToList();
+            return _db.ToDos.ToList();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
